cf23f515118fcc26d9a0a3cc1774992d625334ed
obrien83 <obrien83@yandex.ru>
2019-02-25 21:00:54 +0300

    To Github

---
+++ AxelotTestWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AxelotTestWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
